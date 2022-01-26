277d7f22fe3d082c35a0bae0a4dd3ce483775af0
Hecate <hecate946@gmail.com>
2022-01-25 20:24:38 -0800

    asgi

---
+++ asgi.py
@@ -0,0 +1,8 @@
+from web import app
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
+
+if __name__ == "__main__":
+
+    config = Config.from_mapping(bind=["unix:app.sock"], umask=7)
+    app.loop.run_until_complete(serve(app, config))
