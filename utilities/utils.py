27038604d7702040e3cc85ccde7cc0ddd13a9d3b
Hecatex <74381783+Hecatex@users.noreply.github.com>
2022-02-02 19:15:06 -0800

    update id generator

--- utilities/utils.py
+++ utilities/utils.py
@@ -6,9 +6,12 @@ SKETYL_EPOCH = 1643000000000
 
 def generate_id():
     sketyl_millis = int(datetime.utcnow().timestamp() * 1000 - SKETYL_EPOCH)
-    random_id = "".join(random.choice("0123456789") for _ in range(5))
-    encoded = str((sketyl_millis << 17) + (2 ** 17))
+    random_id = "".join(random.choice("0123456789") for _ in range(3))
+    encoded = str((sketyl_millis << 13) + (2 ** 13))
     return int(encoded + random_id)
+
+
+print(generate_id())
 
 
 def parse_duration(duration: int):
